597c2c34ee1867fa6ba53d4816fbde51ada7ae07
Leonardo <leo.a.pianta@gmail.com>
2023-06-28 16:24:00 -0300

    - Studying unitary tests

---
+++ UnitTests/UnitTests/Contato.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class Contato
+    {
+        public Contato()
+        {               
+        }
+
+        public string nome { get; set; }
+        public string idade { get; set; }
+        public string cpf { get; set; }
+    }
+}
---
+++ UnitTests/UnitTests/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using UnitTests;
+
+internal class Program
+{
+    public static List<Contato> ContatosList = new List<Contato>();
+
+    private static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    private static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("Cadastro de contatos");
+        Console.WriteLine();
+        Console.WriteLine("Opções:");
+        Console.WriteLine("1- Listar");
+        Console.WriteLine("2- Cadastrar");
+        Console.WriteLine("3- Excluir");
+        Console.WriteLine("");        
+
+        var opt = Console.ReadLine();
+
+        switch (opt)
+        {
+            case "1":
+                ListarContatos();
+                break;
+            case "2":
+                CadastrarContato();
+                break;
+            case "3":
+                ExcluirContato();
+                break;
+            default:
+                Menu();
+                break;
+        }
+
+        Menu();
+    }
+
+    private static void ListarContatos()
+    {
+        Console.WriteLine("Contatos cadastrados:");
+        Console.WriteLine();
+
+        foreach (var c in ContatosList)
+        {
+            Console.WriteLine($"Nome: {c.nome}");
+            Console.WriteLine($"Idade: {c.idade}");
+            Console.WriteLine($"Cpf: {c.cpf}");
+            Console.WriteLine();
+        }
+        Console.WriteLine("4- Menu inicial");
+
+        var opt = Console.ReadLine();
+        if (opt.Equals("4"))
+        {
+            Menu();
+        }
+
+    }
+
+    private static void CadastrarContato()
+    {
+        var contato = new Contato();
+
+        Console.WriteLine("Digite o nome:");
+        contato.nome = Console.ReadLine();
+        Console.WriteLine("Digite a idade:");
+        contato.idade = Console.ReadLine();
+        Console.WriteLine("Digite o cpf:");
+        contato.cpf = Console.ReadLine();
+        Console.WriteLine();
+
+        ContatosList.Add(contato);
+    }
+
+    private static void ExcluirContato()
+    {
+        Console.WriteLine("Digite o nome a ser excluído:");
+        var nome = Console.ReadLine().ToLower();
+
+        var contato = ContatosList.FirstOrDefault(x => x.nome.ToLower().Equals(nome));
+        ContatosList.Remove(contato);
+
+        Menu();
+    }
+}
